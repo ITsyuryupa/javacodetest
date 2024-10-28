61cf224aaf44b79478e3a52037cbb9089bcc2590
ITsyuryupa <openingthedoor@yandex.ru>
2024-10-28 20:43:20 +0300

    add model,repository,util package, add wallet model and repository

---
+++ src/main/java/javacode/test/model/Wallet.java
@@ -0,0 +1,34 @@
+package javacode.test.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wallets")
+@Data
+@NoArgsConstructor
+public class Wallet {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "wallet_id", updatable = false, nullable = false)
+    private UUID walletId;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    public Wallet(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
+
---
+++ src/main/java/javacode/test/util/WalletOperationRequest.java
@@ -0,0 +1,24 @@
+package javacode.test.util;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class WalletOperationRequest {
+
+    @NotNull
+    private UUID walletId;
+
+    @NotNull
+    private String operationType;  // "DEPOSIT" или "WITHDRAW"
+
+    @NotNull
+    @Min(value = 0, message = "Amount must be positive")
+    private BigDecimal amount;
+}
+
+
