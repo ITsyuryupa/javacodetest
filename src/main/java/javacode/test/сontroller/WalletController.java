24c8dc1330811c8fdd5e5340db6fdd2a91ea92a2
ITsyuryupa <openingthedoor@yandex.ru>
2024-10-30 15:07:00 +0300

    refactor controller, update GlobalExceptionHandler

--- src/main/java/javacode/test/сontroller/WalletController.java
+++ src/main/java/javacode/test/сontroller/WalletController.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/wallet")
+@RequestMapping("/api/v1/wallet")
 public class WalletController {
 
     private final WalletService walletService;
